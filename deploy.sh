78ddbca117c17e6a79aaf25c205981b46957bf9e
Antoine Hémery <antoine.hemery@algolia.com>
2020-09-05 19:39:16 +0200

    feat: add cname in public instead of sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,6 @@ yarn build
 # navigate into the build output directory
 cd blog/.vuepress/dist
 
-# if you are deploying to a custom domain
-echo 'ahemery.dev' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
